e091a67b5b0bf05f9d7c1321f67abf7049d2da01
DanielCollaguazo2003 <109354364+DanielCollaguazo2003@users.noreply.github.com>
2023-11-22 08:46:30 -0500

    Finalizacion de intalacion del serviceworker y pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -38,7 +38,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
   ],
   providers: [{ provide: FIREBASE_OPTIONS, useValue: environment.firebaseConfig }],
   bootstrap: [AppComponent]
